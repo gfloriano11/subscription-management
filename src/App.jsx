477dfa1d8fc1fb299d0fd7166dd2364cdaefabcc
gfloriano11 <gflorianodev@gmail.com>
2025-03-17 22:05:36 -0300

    feat: project title

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 
 function App() {
 
+  return(
+    <>
+    <div className='bg-gray-950 h-dvh'>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-white mt-2 text-2xl'>Subscription Management</h1>
+      </div>
+    </div>
+    </>
+  )
 
 }
 
