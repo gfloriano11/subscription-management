0ab450036fe6a8bbbcd805068e9637dccb8f2bc6
gfloriano11 <gflorianodev@gmail.com>
2025-03-19 21:14:09 -0300

    chore: change main text

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
     <>
     <div className='bg-gray-950 h-dvh'>
       <div className='flex justify-center items-center'>
-        <h1 className='text-white mt-2 text-2xl'>Subscription Management</h1>
+        <h1 className='text-white mt-5 text-2xl'>Your subscriptions:</h1>
+      </div>
+      <div>
+        
       </div>
     </div>
     </>
