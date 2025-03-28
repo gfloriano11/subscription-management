37940b0511bac74d458b8fdfec6baa3a55d02c98
gfloriano11 <gflorianodev@gmail.com>
2025-03-28 17:53:17 -0300

    feat: import subscription card in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
+import SubscriptionCard from './components/subscription'
 
 function App() {
 
   return(
     <>
-    <div className='bg-gray-950 h-dvh'>
+    <div className='bg-gray-950 h-dvh gap-5 flex flex-col'>
       <div className='flex justify-center items-center'>
         <h1 className='text-white mt-5 text-2xl'>Your subscriptions:</h1>
       </div>
-      <div>
-        
+      <div className='flex justify-center'>
+        <SubscriptionCard/>
       </div>
     </div>
     </>
