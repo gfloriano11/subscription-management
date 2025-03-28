3ffe1383c4c996cd4cf5ac50405a0582f477920c
gfloriano11 <gflorianodev@gmail.com>
2025-03-28 17:55:57 -0300

    style: add inter font in app title

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
     <div className='bg-gray-950 h-dvh gap-5 flex flex-col'>
       <div className='flex justify-center items-center'>
-        <h1 className='text-white mt-5 text-2xl'>Your subscriptions:</h1>
+        <h1 className='text-white mt-5 text-2xl font-inter'>Your subscriptions:</h1>
       </div>
       <div className='flex justify-center'>
         <SubscriptionCard/>
