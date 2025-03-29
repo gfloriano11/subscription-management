f413e0815a41d9a74c21378de0b5b417bd4019a6
gfloriano11 <gflorianodev@gmail.com>
2025-03-29 13:04:43 -0300

    style: add border in card container

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,6 +1,6 @@
 function SubscriptionCard(){
     return(
-        <div className="flex items-center w-2xs bg-zinc-900 p-4 rounded-lg shadow-[0px_10px_20px] shadow-purple-950">
+        <div className="flex items-center w-2xs bg-zinc-900 p-4 rounded-lg shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800">
             <div className="flex gap-5">
                 <div className="flex justify-left items-center p-2 select-none pointer-events-none w-fit border-2 rounded-lg border-gray-500">
                     <img className="w-10" src="src\assets\netflix-logo.svg"></img>
