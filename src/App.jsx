c88aa5f96d61ce63482ef4489298ed31d6a39a88
gfloriano11 <gflorianodev@gmail.com>
2025-03-31 17:35:09 -0300

    feat: form use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import SubscriptionCard from './components/SubscriptionCard'
 import AddSubscription from './components/AddSubscription'
+import Form from './components/Form'
 
 function App() {
+
+  const [form, setForm] = useState(false);
 
   return(
     <>
@@ -11,7 +14,10 @@ function App() {
         <h1 className='text-white mt-5 text-2xl font-inter'>Your subscriptions:</h1>
       </div>
       <div className='flex justify-center'>
-        <AddSubscription/>
+        <AddSubscription form={form} setForm={setForm}/>
+      </div>
+      <div>
+        <Form form={form}/>
       </div>
       <div className='flex justify-center'>
         <SubscriptionCard/>
