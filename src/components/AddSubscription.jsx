241c63066d1f2e80b3af6f388e6e38ab79effdcc
gfloriano11 <gflorianodev@gmail.com>
2025-03-31 17:35:22 -0300

    feat: open and close form

--- src/components/AddSubscription.jsx
+++ src/components/AddSubscription.jsx
@@ -1,9 +1,12 @@
 import { Plus } from "lucide-react";
 
-function AddSubscription(){
+function AddSubscription({form, setForm}){
+
+
     return(
         <button className='text-white border-2 border-slate-300 bg-zinc-900 p-2
-        rounded-lg cursor-pointer select-none font-inter flex justify-center items-center'>
+        rounded-lg cursor-pointer select-none font-inter flex justify-center items-center'
+        onClick={() => setForm(!form)}>
             <Plus color="white"></Plus>
             Add Subscription
         </button>
