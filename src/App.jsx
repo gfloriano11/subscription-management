51a573d10ed06766b13402448fd67fba6f9ff772
gfloriano11 <gflorianodev@gmail.com>
2025-03-31 17:52:03 -0300

    style: add hover on add subs. button

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return(
     <>
-    <div className='bg-gray-950 h-dvh gap-5 flex flex-col'>
+    <div className='bg-slate-950 h-dvh gap-5 flex flex-col'>
       <div className='flex justify-center items-center'>
         <h1 className='text-white mt-5 text-2xl font-inter'>Your subscriptions:</h1>
       </div>
--- src/components/AddSubscription.jsx
+++ src/components/AddSubscription.jsx
@@ -2,10 +2,10 @@ import { Plus } from "lucide-react";
 
 function AddSubscription({form, setForm}){
 
-
     return(
-        <button className='text-white border-2 border-slate-300 bg-zinc-900 p-2
-        rounded-lg cursor-pointer select-none font-inter flex justify-center items-center'
+        <button className='text-white border-2 border-slate-400 bg-zinc-900 p-2
+        rounded-lg cursor-pointer select-none font-inter flex justify-center items-center
+        hover:scale-105 duration-1000 transition-transform will-change-transform'
         onClick={() => setForm(!form)}>
             <Plus color="white"></Plus>
             Add Subscription
