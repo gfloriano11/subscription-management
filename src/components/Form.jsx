9120752be19a2f2637fd9f1550656b48a0368d08
gfloriano11 <gflorianodev@gmail.com>
2025-04-02 17:54:42 -0300

    style: increse form design

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,10 @@
 function Form({form}){
     if(form){
         return(
-            <form className="top-1/2 p-4 rounded-lg fixed flex justify-center bg-white border-2 border-gray-400">
+            <form className="top-1/2 bottom-1/2 p-4 rounded-lg fixed h-fit flex justify-center bg-slate-900 border-2 border-gray-500">
                 <div className="flex flex-col">
-                    <label htmlFor="subscription_name">Nome</label>
-
+                    <label className="text-white" htmlFor="subscription_name">Nome:</label>
+                    <input className="bg-slate-600 rounded-lg h-6 select-none" type="text"></input>
                 </div>
             </form>
         )
