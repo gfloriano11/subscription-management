5da367e04f7af07571984c5cc6fc589fd0bbecd2
gfloriano11 <gflorianodev@gmail.com>
2025-04-02 19:28:58 -0300

    feat: start add subs. form page

--- src/pages/AddSubscriptionForm.jsx
+++ src/pages/AddSubscriptionForm.jsx
@@ -3,7 +3,10 @@ import Form from '../components/Form'
 function AddSubscriptionForm(){
 
     return(
-        <div className='flex justify-center'>
+        <div className='flex flex-col items-center gap-6'>
+            <div>
+                <h1 className='text-white mt-5 text-2xl font-inter'>Add Your Subscription:</h1>
+            </div>
             <Form/>
         </div>
     )
