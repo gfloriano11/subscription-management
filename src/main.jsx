895c090322a238eb2ea3f98718137919519fe67a
gfloriano11 <gflorianodev@gmail.com>
2025-04-02 18:17:46 -0300

    feat: add router in app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <p>Bem-vindo!</p>
+  },
+  {
+    path: '/home',
+    element: <App/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
