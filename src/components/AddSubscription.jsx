585be65d7795fc20a4fe7a858dfbbecf29e06370
gfloriano11 <gflorianodev@gmail.com>
2025-04-02 19:16:52 -0300

    feat: use navigate in add subscription button

--- src/components/AddSubscription.jsx
+++ src/components/AddSubscription.jsx
@@ -1,12 +1,14 @@
 import { Plus } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-function AddSubscription({form, setForm}){
+function AddSubscription(){
+    const navigate = useNavigate();
 
     return(
         <button className='text-white border-2 border-slate-400 bg-zinc-900 p-2
         rounded-lg cursor-pointer select-none font-inter flex justify-center items-center
         hover:scale-105 duration-1000 transition-transform will-change-transform'
-        onClick={() => setForm(!form)}>
+        onClick={() => navigate('add')}>
             <Plus color="white"></Plus>
             Add Subscription
         </button>
