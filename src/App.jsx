d3556e2a2711d8004b7b31dc424bc5ca0a7db4df
gfloriano11 <gflorianodev@gmail.com>
2025-04-02 19:15:34 -0300

    refacture: add outlet and remove other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from 'react'
-import SubscriptionCard from './components/SubscriptionCard'
-import AddSubscription from './components/AddSubscription'
-import Form from './components/Form'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-
-  const [form, setForm] = useState(false);
 
   return(
     <>
     <div className='bg-slate-950 h-dvh gap-5 flex flex-col'>
-      <div className='flex justify-center items-center'>
-        <h1 className='text-white mt-5 text-2xl font-inter'>Your subscriptions:</h1>
-      </div>
-      <div className='flex justify-center'>
-        <AddSubscription form={form} setForm={setForm}/>
-      </div>
-      <div className='flex justify-center'>
-        <Form form={form}/>
-      </div>
-      <div className='flex justify-center'>
-        <SubscriptionCard/>
-      </div>
+      <Outlet/>
     </div>
     </>
   )
