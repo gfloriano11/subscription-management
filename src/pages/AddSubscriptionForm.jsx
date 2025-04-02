b9ebca460e3ba74c1de43283efeca98271e6a8bc
gfloriano11 <gflorianodev@gmail.com>
2025-04-02 19:16:29 -0300

    feat: create form page

---
+++ src/pages/AddSubscriptionForm.jsx
@@ -0,0 +1,12 @@
+import Form from '../components/Form'
+
+function AddSubscriptionForm(){
+
+    return(
+        <div className='flex justify-center'>
+            <Form/>
+        </div>
+    )
+}
+
+export default AddSubscriptionForm;
