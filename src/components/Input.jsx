9d0b8a4e973392f0a318b05b5e777ffa524e1a2b
gfloriano11 <gflorianodev@gmail.com>
2025-04-03 21:19:34 -0300

    feat: create input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+function Input({type}){
+    return(
+        <input className="bg-slate-600 rounded-lg h-6 select-none outline-hidden" 
+        type={type}>
+        </input>
+    )
+}
+
+export default Input
