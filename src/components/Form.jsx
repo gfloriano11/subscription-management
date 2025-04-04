40473e55372a6182197485f4f489f9858f420209
gfloriano11 <gflorianodev@gmail.com>
2025-04-03 21:19:51 -0300

    feat: use input component at form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,15 @@
+import Input from '../components/Input';
+
 function Form(){
 
     return(
         <form className="p-4 rounded-lg h-fit flex justify-center bg-slate-900 border-2 border-gray-500">
              <div className="flex flex-col">
-                   <label className="text-white" htmlFor="subscription_name">Nome:</label>
-                <input className="bg-slate-600 rounded-lg h-6 select-none" type="text"></input>
+                <label className="text-white" htmlFor="subscription_name">Nome:</label>
+                <Input type="text"/>
+            </div>
+            <div>
+                
             </div>
         </form>
     )
