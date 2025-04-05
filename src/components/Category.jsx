b1b2b5259e8f93647e6ed2ad7db001293a8feb00
gfloriano11 <gflorianodev@gmail.com>
2025-04-05 15:03:09 -0300

    style: decrease font weight

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,9 @@
 function Category({id, text}){
     return(
-        <div id={id} className="flex justify-center w-2xs bg-zinc-900 p-4 rounded-lg 
+        <div id={id} className="flex justify-center w-58 bg-zinc-900 p-4 rounded-lg 
         shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
         gap-2.5 text-white font-inter font-light">
-            <p className="font-inter text-3xl font-bold">{text}</p>
+            <p className="font-inter text-3xl font-medium">{text}</p>
         </div>
     );
 }
