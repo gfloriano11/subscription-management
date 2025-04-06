670e9d53544c504c30f203278a1cc42bee9af01e
gfloriano11 <gflorianodev@gmail.com>
2025-04-06 10:36:54 -0300

    feat: select category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 function Category({id, text}){
+
+    const navigate = useNavigate();
     return(
-        <div id={id} className="flex justify-center w-58 bg-zinc-900 p-4 rounded-lg 
+        <button id={id}
+        onClick={() => navigate('teste')}
+        className="flex justify-center w-58 bg-zinc-900 p-4 rounded-lg 
         shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
-        gap-2.5 text-white font-inter font-light">
+        gap-2.5 text-white font-inter font-light cursor-pointer select-none hover:scale-105">
             <p className="font-inter text-3xl font-medium">{text}</p>
-        </div>
+        </button>
     );
 }
 
