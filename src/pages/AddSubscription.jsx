172f1c3761ebf72c692bace3fff3f620c20ff974
gfloriano11 <gflorianodev@gmail.com>
2025-04-07 14:54:49 -0300

    feat: add router logic to categories

--- src/pages/AddSubscription.jsx
+++ src/pages/AddSubscription.jsx
@@ -1,6 +1,25 @@
+import { useLocation, useParams } from 'react-router-dom';
 import CreateSubscriptionForm from '../components/CreateSubscriptionForm'
 
 function AddSubscription(){
+
+    const location = useLocation();
+    const { pathname } = location;
+    const uri = pathname.split('/');
+    const category = uri[3];
+
+    if(category !== 'custom'){
+        console.log('não é custom');
+        if(category === 'streaming' || category === 'games' || category === 'education' || category === 'music' || category === 'healthcare'){
+            console.log('é fixo');
+        } else {
+            return(
+                <p className='text-white'>erro</p>
+            )
+        }
+    } else {
+        console.log('é custom');
+    }
 
     return(
         <div className='flex flex-col items-center gap-6'>
