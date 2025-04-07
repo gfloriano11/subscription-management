660864fca19914e5460a59d66a667a6fd5383343
gfloriano11 <gflorianodev@gmail.com>
2025-04-07 17:04:25 -0300

    feat: create go back button

---
+++ src/components/GoBackButton.jsx
@@ -0,0 +1,19 @@
+import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom"
+
+function GoBackButton(){
+
+    const navigate = useNavigate();
+
+    return(
+        <button onClick={() => navigate(-1)}
+        className="text-white border-2 border-slate-400 bg-zinc-900 p-2
+        rounded-lg cursor-pointer select-none font-inter flex justify-center items-center
+        hover:scale-105 duration-1000 transition-transform will-change-transform'">
+            <ArrowLeft color="white"/>
+            <p className="text-white">Go Back</p>
+        </button>
+    );
+}
+
+export default GoBackButton;
