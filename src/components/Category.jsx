dd941b08c50af6a06432c6d378e8991f83a25c77
gfloriano11 <gflorianodev@gmail.com>
2025-04-07 14:54:19 -0300

    fix: change category to Link component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,16 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function Category({id, text}){
+function Category({id, text, path}){
 
-    const navigate = useNavigate();
     return(
-        <button id={id}
-        onClick={() => navigate('teste')}
+        <Link id={id}
+        to={path}
         className="flex justify-center w-58 bg-zinc-900 p-4 rounded-lg 
         shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
         gap-2.5 text-white font-inter font-light cursor-pointer select-none hover:scale-105">
             <p className="font-inter text-3xl font-medium">{text}</p>
-        </button>
+        </Link>
     );
 }
 
