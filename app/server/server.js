fd53818afccd89de1a2aefcdff9c19c05d0c9235
gfloriano11 <gflorianodev@gmail.com>
2025-04-09 17:55:01 -0300

    feat: create route to category

--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import router from '../router/home.js';
+import categoryRouter from '../router/category.js';
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', router);
+app.get('/', categoryRouter);
 
 app.listen(8000, () => {
     console.log('server running at http://localhost:8000')
