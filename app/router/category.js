f7823e65a6ff171ee5e9e121b900a1dac2d85f84
gfloriano11 <gflorianodev@gmail.com>
2025-04-09 17:55:18 -0300

    feat: use get category func

--- app/router/category.js
+++ app/router/category.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { category } from '../model/category';
+import category from '../model/category.js';
 
 const categoryRouter = express.Router();
 
-categoryRouter.get('/', category);
+categoryRouter.get('/', category.getCategory);
 
 export default categoryRouter;
