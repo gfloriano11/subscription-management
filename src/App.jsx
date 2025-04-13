cd1b9e7e3ca349d02e76087933a8c14a4a93e51e
gfloriano11 <gflorianodev@gmail.com>
2025-04-12 22:44:18 -0300

    chore: fix identation

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
 
   return(
     <>
-    <div className='bg-slate-950 h-dvh gap-5 flex justify-center'>
-      <Outlet/>
-    </div>
+      <div className='bg-slate-950 h-dvh gap-5 flex justify-center'>
+        <Outlet/>
+      </div>
     </>
   )
 
