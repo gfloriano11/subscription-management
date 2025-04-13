29bf1ee2d9ec18a6bb726943f1a6d7587cfa090b
gfloriano11 <gflorianodev@gmail.com>
2025-04-13 11:14:52 -0300

    feat: start menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Menu(){
 
     return(
-        <div>
+        <div className="flex flex-col justify-center items-center h-dvh">
             <div>
                 <p>Welcome to Subscription Management!</p>
+            </div>
+            <div className="flex gap-2">
+                <Link
+                to={'/home'}>
+                    <p>Login</p>
+                </Link>
+                <Link
+                to={'/home'}>
+                    <p>Register</p>
+                </Link>
             </div>
         </div>
     )
