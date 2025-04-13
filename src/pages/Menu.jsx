a2d787bfaa8f14b00077ddada22e5386306caad8
gfloriano11 <gflorianodev@gmail.com>
2025-04-12 22:44:42 -0300

    feat: create menu page

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,12 @@
+function Menu(){
+
+    return(
+        <div>
+            <div>
+                <p>Welcome to Subscription Management!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
