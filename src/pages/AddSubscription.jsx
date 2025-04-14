f9a25862201ee8b09129986f82acb236a8539ce7
gfloriano11 <gflorianodev@gmail.com>
2025-04-14 13:24:44 -0300

    feat: fetch subscriptions with category

--- src/pages/AddSubscription.jsx
+++ src/pages/AddSubscription.jsx
@@ -1,40 +1,53 @@
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import CreateSubscriptionForm from '../components/CreateSubscriptionForm'
 import GoBackButton from '../components/GoBackButton';
 
 function AddSubscription(){
+
+    const [subscriptions, setSubscriptions] = useState([]);
 
     const location = useLocation();
     const { pathname } = location;
     const uri = pathname.split('/');
     const category = uri[3];
 
+    async function getSubscription(){
+
+        try{
+            const response = await fetch(`http://localhost:8000/subscription/${category}`, {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+
+            if(!response.ok){
+                throw new Error('Error to fetch, subscriptions');
+            }
+
+            let data = await response.json();
+
+            setSubscriptions(data);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+
     if(category !== 'custom'){
         if(category === 'streaming' || category === 'games' || category === 'education' || category === 'music' || category === 'healthcare'){
 
-            async function getSubscription(){
-
-                try{
-                    const response = await fetch(`http://localhost:8000/`, {
-                        method: 'GET',
-                        headers: {
-                            'Content-type': 'application/json'
-                        },
-                        body: {
-                            categoryId: '1'
-                        }
-                    })
-
-                } catch (error){
-                    console.error(error);
-                }
-            }
         } else {
             return(
                 <p className='text-white'>erro</p>
             )
         }
     }
+
+    useEffect(() => {
+        getSubscription();
+    }, []);
 
     return(
         <div className='flex flex-col items-center gap-6'>
