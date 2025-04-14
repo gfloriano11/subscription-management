1c2399ca5479612ec428efb6495f4d8125a8b11b
gfloriano11 <gflorianodev@gmail.com>
2025-04-14 19:03:39 -0300

    style: add background color in subscription container

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,12 +1,12 @@
 function Subscription({id, text, path}){
 
     return(
-        <div id={id} className="border-2 border-white rounded-3xl flex flex-col">
-            <div className="w-50">
-                <img className="rounded-3xl" src="https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5..v1582751026.png"></img>
+        <div id={id} className="border-2 border-gray-700 bg-gray-800 rounded-3xl flex flex-col cursor-pointer">
+            <div className="w-50 select-none pointer-events-none">
+                <img className="rounded-tl-3xl rounded-tr-3xl pointer-events-none" src="https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5..v1582751026.png"></img>
             </div>
-            <div className="text-center">
-                <p className="text-white">{text}</p>
+            <div className="w-full text-center rounded-bl-3xl rounded-br-3xl">
+                <p className="text-white select-none">{text}</p>
             </div>
         </div>
     );
