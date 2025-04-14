44dfe649beabf482546f94d03f80211c1d18667f
gfloriano11 <gflorianodev@gmail.com>
2025-04-14 14:02:55 -0300

    feat: create route to subscription

--- app/server/server.js
+++ app/server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import router from '../router/home.js';
 import categoryRouter from '../router/category.js';
+import subscriptionRouter from '../router/subscription.js';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 
 app.get('/', router);
 app.use('/category', categoryRouter);
+app.use('/subscription', subscriptionRouter);
 
 app.listen(8000, () => {
     console.log('server running at http://localhost:8000')
