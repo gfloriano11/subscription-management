6eb2c7fa25dccf798ea18b9d6e0e29e9be5373e5
gfloriano11 <gflorianodev@gmail.com>
2025-04-15 13:45:02 -0300

    style: add hover in subscription

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -2,14 +2,14 @@ function Subscription({id, text, path}){
 
     return(
         <div id={id} className="group relative border-2 border-gray-700 bg-gray-800 
-        rounded-3xl flex flex-col cursor-pointer overflow-hidden transition-all duration-500 
-        h-36">
+        rounded-3xl flex flex-col cursor-pointer overflow-hidden transition-all duration-300 
+        h-36 hover:scale-102">
             <div className="w-60 h-full select-none pointer-events-none flex items-center">
                 <img className="rounded-tl-3xl rounded-tr-3xl pointer-events-none center object-cover h-40" src="https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5..v1582751026.png"></img>
             </div>
             <div className="absolute bottom-0 w-full opacity-0 group-hover:opacity-100 
             flex justify-center bg-zinc-900 p-0.5 transition-all duration-500
-            ease-in-out">
+            ease-in-out select-none">
                 <p className="text-white text-lg tracking-wide">Netflix</p>
             </div>
         </div>
