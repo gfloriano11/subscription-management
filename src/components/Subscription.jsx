c472eaafdee7cb9d665dd1d733eceb0fedd662de
gfloriano11 <gflorianodev@gmail.com>
2025-04-16 15:22:37 -0300

    style: increase subscription container

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -3,7 +3,7 @@ function Subscription({id, text, path, image}){
     return(
         <div id={id} className="group relative border-2 border-gray-700 bg-gray-800 
         rounded-3xl flex flex-col cursor-pointer overflow-hidden transition-all duration-300 
-        h-36 w-60.5 hover:scale-101 will-change-transform">
+        h-36 w-65 hover:scale-101 will-change-transform">
             <div className="w-full h-full select-none pointer-events-none flex items-center">
                 <img className="rounded-tl-3xl rounded-tr-3xl pointer-events-none center object-cover w-full h-full" src={image}></img>
             </div>
