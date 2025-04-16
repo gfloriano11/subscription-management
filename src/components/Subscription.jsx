6bb8caa849b3b0c224f23a6787e3adc3e5a3efd6
gfloriano11 <gflorianodev@gmail.com>
2025-04-16 16:30:13 -0300

    chore: remove border radius from image

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,11 +1,11 @@
 function Subscription({id, text, path, image}){
 
     return(
-        <div id={id} className="group relative border-2 border-gray-700 bg-gray-800 
+        <div id={id} className="group relative border-2 border-gray-700 bg-white 
         rounded-3xl flex flex-col cursor-pointer overflow-hidden transition-all duration-300 
         h-36 w-65 hover:scale-101 will-change-transform">
             <div className="w-full h-full select-none pointer-events-none flex items-center">
-                <img className="rounded-tl-3xl rounded-tr-3xl pointer-events-none center object-cover w-full h-full" src={image}></img>
+                <img className="pointer-events-none center object-cover w-full h-full" src={image}></img>
             </div>
             <div className="absolute bottom-0 w-full opacity-0 group-hover:opacity-100 
             flex justify-center bg-zinc-900 p-0.5 transition-all duration-500
