d478f695fb4d1d87a194148f87c40ec89fc792e5
gfloriano11 <gflorianodev@gmail.com>
2025-04-17 17:44:10 -0300

    feat: create page to add subscription info

---
+++ src/pages/AddSubscriptionInfo.jsx
@@ -0,0 +1,9 @@
+function AddSubscriptionInfo(){
+    return(
+        <div>
+            <p>add *subscription name* information:</p>
+        </div>
+    );
+}
+
+export default AddSubscriptionInfo;
