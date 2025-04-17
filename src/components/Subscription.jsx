98603ed1f9c82280b8be0b07171385759314cf3f
gfloriano11 <gflorianodev@gmail.com>
2025-04-17 17:58:42 -0300

    fix: change subscription compoent to Link

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Subscription({id, text, path, image}){
 
     return(
-        <div id={id} className="group relative border-2 border-gray-700 bg-white 
+        <Link
+        id={id}
+        to={path} 
+        className="group relative border-2 border-gray-700 bg-white 
         rounded-3xl flex flex-col cursor-pointer overflow-hidden transition-all duration-300 
         h-36 w-65 hover:scale-101 will-change-transform">
             <div className="w-full h-full select-none pointer-events-none flex items-center">
@@ -12,7 +17,7 @@ function Subscription({id, text, path, image}){
             ease-in-out select-none">
                 <p className="text-white text-lg tracking-wide">{text}</p>
             </div>
-        </div>
+        </Link>
     );
 }
 
