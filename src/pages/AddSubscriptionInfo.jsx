6cd1e8bd9562303eac1e37195580792c988dc217
gfloriano11 <gflorianodev@gmail.com>
2025-04-19 12:05:45 -0300

    feat: get subscription info in front

--- src/pages/AddSubscriptionInfo.jsx
+++ src/pages/AddSubscriptionInfo.jsx
@@ -1,34 +1,43 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function AddSubscriptionInfo(){
 
+    const [subscription, setSubscription] = useState({});
+
     const location = useLocation();
     const id = location.state.id;
 
-    async function getSubscription(id){
+    async function getSubscriptionById(id){
 
-        const response = await fetch(`http://localhost:8000/subscription/${id}`, {
+
+        const response = await fetch(`http://localhost:8000/subscription/id/${id}`, {
             method: 'GET',
             headers: {
                 'Content-type': 'application/json'
             }
         });
 
-        console.log(response);
-
         if(!response.ok){
             throw new Error('Error to fetch subscription information', response);
         }
 
         const data = await response.json();
+        // console.log(data);
+        setSubscription(data[0]);
     }
+
+    useEffect(() => {
+        getSubscriptionById(id);
+    }, []);
 
     return(
         <div className="text-white">
             <div>
-                <p>add {id} information:</p>
+                <p>add {subscription.subscription_name} information:</p>
             </div>
-            <div>
+            <div className="text-white">
+                <p>{subscription.subscription_name}</p>
                 <div>
                     <img></img>
                 </div>
