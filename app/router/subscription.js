b9813b1e69ce09c031f4c774e86dddafa708f903
gfloriano11 <gflorianodev@gmail.com>
2025-04-19 12:05:11 -0300

    feat: add route to get subscription by id

--- app/router/subscription.js
+++ app/router/subscription.js
@@ -4,5 +4,6 @@ import subscription from '../model/subscription.js';
 const subscriptionRouter = express();
 
 subscriptionRouter.get('/:category', subscription.getSubscription);
+subscriptionRouter.get('/id/:id', subscription.getSubscriptionById);
 
 export default subscriptionRouter;
