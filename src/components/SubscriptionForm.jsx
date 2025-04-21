76f6476c5f5af8844d73432c56507d824b8787b0
gfloriano11 <gflorianodev@gmail.com>
2025-04-20 21:57:21 -0300

    style: add input style

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -2,10 +2,19 @@ function SubscriptionForm(){
 
     return(
         <div className="bg-slate-800 border-1 border-gray-700 rounded-lg">
-            <form>
-                <div>
+            <form className="p-3">
+                <div className="flex flex-col">
                     <label htmlFor="subscription-name">Subscription:</label>
-                    <input type="text" name="subscription-name" value="Netflix"/>
+                    <input 
+                    type="text" 
+                    name="subscription-name" 
+                    value="Netflix"
+                    className="bg-white p-1.5 flex items-center text-black
+                    rounded-xl"/>
+                </div>
+                <div>
+                    <label htmlFor="subscription-value">Price:</label>
+                    <input type="text" name="subscription-value" value="USD 12"/>
                 </div>
             </form>
         </div>
