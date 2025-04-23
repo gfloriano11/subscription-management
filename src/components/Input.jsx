405463a9a57a422f62de4dad30639efd00ec5c72
gfloriano11 <gflorianodev@gmail.com>
2025-04-23 17:23:49 -0300

    feat: input uses data from subscription

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,21 +1,12 @@
-function Input({type, name, custom}){
-
-    if(custom === 0){
-        return(
-            <input 
-            type={type} 
-            name={name} 
-            value='oi'
-            className="bg-white p-1.5 flex items-center text-black
-            rounded-xl"/>
-        )
-    }
+function Input({type, name, value, custom, onChange}){
 
     return(
         <input 
         type={type} 
         name={name} 
-        value='cu'
+        value={value}
+        readOnly={custom}
+        onChange={onChange}
         className="bg-white p-1.5 flex items-center text-black
         rounded-xl"/>
     )
