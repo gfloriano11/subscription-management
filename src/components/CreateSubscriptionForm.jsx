e8050322121c25367e0aeae8e6bf37547520a032
gfloriano11 <gflorianodev@gmail.com>
2025-04-23 17:37:56 -0300

    feat: add input info in create subsc. form

--- src/components/CreateSubscriptionForm.jsx
+++ src/components/CreateSubscriptionForm.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Input from './Input';
 
 function CreateSubscriptionForm(){
 
+    const [name, setName] = useState('');
+    const [isCustom, setIsCustom] = useState(1);
+
     return(
         <form className="p-4 rounded-lg h-fit flex justify-center bg-slate-900 border-2 border-gray-500">
-             <div className="flex flex-col">
+            <div className="flex flex-col">
                 <label className="text-white" htmlFor="subscription_name">Nome:</label>
-                <Input type="text"/>
+                <Input
+                type="text"
+                name="subscription-name"
+                custom={isCustom}
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                />
             </div>
             <div>
                 
