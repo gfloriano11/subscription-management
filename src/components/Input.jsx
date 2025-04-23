ad564d109ab6ca856283fc8ebf9255cb05eadc0e
gfloriano11 <gflorianodev@gmail.com>
2025-04-23 16:56:37 -0300

    feat: verify if subscription is a custom subscription or not

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,21 @@
-function Input({type, name, value}){
+function Input({type, name, custom}){
+
+    if(custom === 0){
+        return(
+            <input 
+            type={type} 
+            name={name} 
+            value='oi'
+            className="bg-white p-1.5 flex items-center text-black
+            rounded-xl"/>
+        )
+    }
+
     return(
         <input 
         type={type} 
         name={name} 
-        value={value}
+        value='cu'
         className="bg-white p-1.5 flex items-center text-black
         rounded-xl"/>
     )
