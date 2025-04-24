36b5c0c8487bca6555d0f4fb87e6c0c3fd3e16ca
gfloriano11 <gflorianodev@gmail.com>
2025-04-24 16:52:28 -0300

    fix: change is custom to true by default

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -4,7 +4,7 @@ import Input from "./Input";
 function SubscriptionForm({subscription}){
 
     const [name, setName] = useState('');
-    const [isCustom, setIsCustom] = useState(0);
+    const [isCustom, setIsCustom] = useState(1);
 
     useEffect(() => {
         if (subscription) {
@@ -15,7 +15,7 @@ function SubscriptionForm({subscription}){
 
     return(
         <div className="bg-slate-800 border-1 border-gray-700 rounded-lg">
-            <form className="p-3">
+            <form className="p-3 text-white">
                 <div className="flex flex-col">
                     <label htmlFor="subscription-name">Subscription:</label>
                     <Input
