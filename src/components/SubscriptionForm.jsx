487943083b70587051f1a3a62d5d9b50522759fc
gfloriano11 <gflorianodev@gmail.com>
2025-04-25 18:08:11 -0300

    style: centralize form in screen

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -14,10 +14,14 @@ function SubscriptionForm({subscription}){
     }, [subscription]);
 
     return(
-        <div className="bg-slate-800 border-1 border-gray-700 rounded-lg">
+        <div className="flex bg-slate-800 border-1 border-gray-700 rounded-lg
+        w-3/4">
+            <div className="w-1/2 select-none pointer-events-none">
+                <img className="rounded-lg" src="\src\assets\subscription_image\netflix.png"></img>
+            </div>
             <form className="p-3 text-white">
                 <div className="flex flex-col">
-                    <label htmlFor="subscription-name">Subscription:</label>
+                    <label htmlFor="subscription-name">Subscription Name:</label>
                     <Input
                     type="text"
                     name="subscription-name"
@@ -25,6 +29,7 @@ function SubscriptionForm({subscription}){
                     value={name}
                     onChange={(event) => setName(event.target.value)}/>
                 </div>
+                
                 <div>
                     <label htmlFor="subscription-value">Price:</label>
                     <input type="text" name="subscription-value" value="USD 12"/>
