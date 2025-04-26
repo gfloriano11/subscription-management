f25ae6001a42fbc970c1e0573a6ed5d6f96d416c
gfloriano11 <gflorianodev@gmail.com>
2025-04-26 12:21:38 -0300

    feat: add others inputs

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -6,6 +6,19 @@ function SubscriptionForm({subscription}){
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
     const [isCustom, setIsCustom] = useState(1);
+    const [error, setError] = useState('');
+
+    function verifyPrice(price){
+        const value = price.target.value;
+
+        setPrice(value)
+
+        if(isNaN(value) || value < 0){
+            setError('Please, write a valid number.');
+        } else {
+            setError('');
+        }
+    }
 
     useEffect(() => {
         if (subscription) {
@@ -21,23 +34,71 @@ function SubscriptionForm({subscription}){
                 <img className="rounded-lg" src="\src\assets\subscription_image\netflix.png"></img>
             </div>
             <form className="p-3 text-white">
-                <div className="flex flex-col">
-                    <label htmlFor="subscription-name">Subscription Name:</label>
-                    <Input
-                    type="text"
-                    name="subscription-name"
-                    custom={isCustom}
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="subscription-value">Price:</label>
-                    <Input
-                    type="text"
-                    name="subscription-value"
-                    custom={true}
-                    value={price}
-                    onChange={(event) => setPrice(event.target.value)}/>
+                <div className="grid grid-cols-2 gap-x-5 gap-y-3">
+                    <div className="flex flex-col">
+                        <label htmlFor="subscription-name">Subscription Name:</label>
+                        <Input
+                        type="text"
+                        name="subscription-name"
+                        custom={!isCustom}
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="users-number">Users:</label>
+                        <Input
+                        type="text"
+                        name="users-number"
+                        custom={true}
+                        value={price}
+                        onChange={(event) => setPrice(event.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor="subscription-value">Price:</label>
+                        <Input
+                        type="text"
+                        name="subscription-value"
+                        custom={true}
+                        value={price}
+                        onChange={verifyPrice}/>
+                        {error && (
+                            <p className="text-red-600">{error}</p>
+                        )}
+                    </div>
+                    <div>
+                        <label htmlFor="start-date">Start Date:</label>
+                        <Input
+                        type="date"
+                        name="start-date"
+                        custom={true}
+                        value={price}
+                        onChange={(event) => {event.target.value}}/>
+                    </div>
+                    <div>
+                        <label htmlFor="due-date">Due Date:</label>
+                        <Input
+                        type="date"
+                        name="due-date"
+                        custom={true}
+                        value={price}
+                        onChange={(event) => {event.target.value}}/>
+                    </div>
+                    <div>
+                        <label htmlFor="payment-method">Payment Method:</label>
+                        <Input
+                        type="select"
+                        name="payment-method"
+                        custom={true}
+                        value={price}
+                        onChange={(event) => setPrice(event.target.value)}
+                        >
+                            <option>Credit Card</option>
+                            <option>Debit Card</option>
+                            <option>PayPal</option>
+                            <option>Apple Pay</option>
+                            <option>Google Pay</option>
+                        </Input>
+                    </div>
                 </div>
             </form>
         </div>
