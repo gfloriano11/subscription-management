a448b6b4a4745bc0e56484559c11dbdbdc61c2b1
gfloriano11 <gflorianodev@gmail.com>
2025-04-26 11:56:10 -0300

    feat: verify if input is a select

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,23 @@
-function Input({type, name, value, custom, onChange}){
+function Input({type, name, value, custom, onChange, children}){
     
     let permission = "bg-slate-400 cursor-not-allowed"
     if(custom === true){
         permission = 'bg-slate-200'
     }
+
+    if (type === "select") {
+        return (
+          <select
+            name={name}
+            value={value}
+            onChange={onChange}
+            className={`${permission} p-1.5 flex items-center text-black rounded-lg outline-none w-full`}
+          >
+            {children}
+          </select>
+        );
+      }
+
     return(
         <input 
         type={type} 
@@ -12,7 +26,7 @@ function Input({type, name, value, custom, onChange}){
         readOnly={!custom}
         onChange={onChange}
         className={`${permission} p-1.5 flex items-center text-black
-        rounded-lg outline-none`}/>
+        rounded-lg outline-none w-full`}/>
     )
 }
 
