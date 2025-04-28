4d228239eff1cccd658002c68ea5f797579183dc
gfloriano11 <gflorianodev@gmail.com>
2025-04-28 17:50:29 -0300

    style: submit button

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,12 @@
 function SubmitButton(){
 
     return(
-        <button>
-            Save
-        </button>
+        <div className="flex justify-center">
+            <button type="button"
+            className="w-1/5 p-2 bg-purple-950 border-white border-1 rounded-xl cursor-pointer">
+                <p>Save</p>
+            </button>
+        </div>
     );
 }
 
