9d6401450ac6fac8c3e8896a72b627c7b16d6c17
gfloriano11 <gflorianodev@gmail.com>
2025-04-30 16:14:31 -0300

    chore: fix text size in submit button hover

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -2,7 +2,7 @@ function SubmitButton(){
 
     return(
         <button type="button"
-        className="w-40 p-2 h-10 bg-purple-700 hover:bg-purple-800 hover:text-[17px] hover:scale-105 transition-all 
+        className="w-40 p-2 h-10 bg-purple-700 hover:bg-purple-800 hover:text-[16px] hover:scale-105 transition-all 
         duration-300 ease-in-out text-white rounded-xl cursor-pointer will-change-transform">
             <p>Add Subscription</p>
         </button>
