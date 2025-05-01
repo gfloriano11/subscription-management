c115a5aad83887b35d107cdb598314823af09c8f
gfloriano11 <gflorianodev@gmail.com>
2025-05-01 15:19:56 -0300

    fix: change button type

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,7 +1,7 @@
 function SubmitButton(){
 
     return(
-        <button type="button"
+        <button
         className="w-40 p-2 h-10 bg-purple-700 hover:bg-purple-800 hover:text-[16px] hover:scale-105 transition-all 
         duration-300 ease-in-out text-white rounded-xl cursor-pointer will-change-transform">
             <p>Add Subscription</p>
