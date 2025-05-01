82a6a16c1a42cb40a697ac67a039c77345fa4cae
gfloriano11 <gflorianodev@gmail.com>
2025-05-01 14:16:27 -0300

    feat: use ref in input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-function Input({type, name, value, custom, onChange, children}){
+function Input({type, name, value, custom, onChange, children, ref}){
     
     let permission = "bg-slate-400 cursor-not-allowed"
     if(custom === 1){
@@ -12,6 +12,7 @@ function Input({type, name, value, custom, onChange, children}){
             value={value}
             onChange={onChange}
             className={`${permission} p-1.5 flex items-center text-black rounded-lg outline-none w-full`}
+            ref={ref}
           >
             {children}
           </select>
@@ -25,6 +26,7 @@ function Input({type, name, value, custom, onChange, children}){
         value={value}
         readOnly={!custom}
         onChange={onChange}
+        ref={ref}
         className={`${permission} p-1.5 h-9 items-center text-black
         rounded-lg outline-none w-full`}/>
     )
