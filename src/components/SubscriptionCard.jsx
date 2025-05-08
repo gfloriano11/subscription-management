a46a1576125997b8af273f5d8fcd17b65af73ca6
gfloriano11 <gflorianodev@gmail.com>
2025-05-08 17:23:38 -0300

    feat: subscription component pull props

--- src/components/SubscriptionCard.jsx
+++ src/components/SubscriptionCard.jsx
@@ -1,8 +1,10 @@
 import { ArrowUpRight } from "lucide-react";
 
-function SubscriptionCard(){
+function SubscriptionCard({id, subscriptionName, subscriptionPrice}){
     return(
-        <div className="flex items-center w-2xs bg-zinc-900 p-4 rounded-lg 
+        <div
+        subscriptionId={id}
+        className="flex items-center w-2xs bg-zinc-900 p-4 rounded-lg 
         shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
         gap-2.5 text-white font-inter font-light">
             <div className="flex gap-5">
@@ -13,10 +15,10 @@ function SubscriptionCard(){
             <div className="flex justify-between w-5/7">
                 <div className="flex flex-col">
                     <div className="flex items-center">
-                        <p className="text-white text-2xl">Netflix</p>
+                        <p className="text-white text-2xl">{subscriptionName}</p>
                     </div>
                     <div className="flex justify-baseline">
-                        <p>R$99.90</p>
+                        <p>{subscriptionPrice}</p>
                     </div>
                 </div>
                 <a className="flex items-center">
