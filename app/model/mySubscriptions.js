b79f48df54209abd6b98c11b3f53342d6ef1d054
gfloriano11 <gflorianodev@gmail.com>
2025-05-08 17:00:51 -0300

    feat: create function getSubscriptions

---
+++ app/model/mySubscriptions.js
@@ -0,0 +1,26 @@
+import connect from "../connection/connection.js";
+
+function getSubscriptions(req, res){
+
+    const connection = connect.getConnection();
+    
+    const query = `SELECT m.id, m.subscription_path, m.subscription_path,
+        m.price, m.users, m.due_date, m.start_date, m.payment_method, m.category_id, 
+        m.image
+        FROM my_subscriptions AS m;`
+
+    connection.query(query, (error, data) => {
+
+        if(error){
+            return res.status(500).json(error);
+        }
+
+        res.status(200).json(data);
+    });
+
+    connect.endConnection(connection);
+}
+
+export default {
+    getSubscriptions
+}
