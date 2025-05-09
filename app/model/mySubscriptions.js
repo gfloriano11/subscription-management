91e60ab45f1629efa1f23b47424fb1cad57f6a8f
gfloriano11 <gflorianodev@gmail.com>
2025-05-09 15:02:18 -0300

    fix: query to get my subscriptions pull image and logo

--- app/model/mySubscriptions.js
+++ app/model/mySubscriptions.js
@@ -5,8 +5,8 @@ function getSubscriptions(req, res){
     const connection = connect.getConnection();
     
     const query = `SELECT m.id, m.subscription_name, m.subscription_path,
-        m.price, m.users, m.due_date, m.start_date, m.payment_method, m.category_id, 
-        m.image
+        m.price, m.users, m.due_date, m.start_date, m.payment_method, m.logo, 
+        m.image, m.category_id, m.image
         FROM my_subscriptions AS m;`
 
     connection.query(query, (error, data) => {
