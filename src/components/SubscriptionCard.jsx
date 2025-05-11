f483abe5966d121406d40de673c7750b5b76a209
gfloriano11 <gflorianodev@gmail.com>
2025-05-11 10:18:34 -0300

    fix: subscription id

--- src/components/SubscriptionCard.jsx
+++ src/components/SubscriptionCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function SubscriptionCard({id, subscriptionName, subscriptionPrice, currency, path, logo}){
     return(
         <div
-        subscriptionId={id}
+        id={id}
         className="flex items-center w-2xs bg-zinc-900 p-4 rounded-lg 
         shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
         gap-2.5 text-white font-inter font-light">
