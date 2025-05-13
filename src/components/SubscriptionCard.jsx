f481925d665127700dfc51c02ff377ea964b87da
gfloriano11 <gflorianodev@gmail.com>
2025-05-13 15:12:17 -0300

    chore: add rounded in logo image

--- src/components/SubscriptionCard.jsx
+++ src/components/SubscriptionCard.jsx
@@ -2,6 +2,7 @@ import { ArrowUpRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
 function SubscriptionCard({id, subscriptionName, subscriptionPrice, currency, path, logo}){
+
     return(
         <div
         id={id}
@@ -9,8 +10,8 @@ function SubscriptionCard({id, subscriptionName, subscriptionPrice, currency, pa
         shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
         gap-2.5 text-white font-inter font-light">
             <div className="flex gap-5">
-                <div className="flex justify-left items-center p-2 select-none pointer-events-none w-fit border-2 rounded-lg border-gray-500">
-                    <img className="w-10" src={logo}></img>
+                <div className="flex justify-left items-center select-none p-2 pointer-events-none w-fit border-2 rounded-lg border-gray-500">
+                    <img className="w-10 rounded-lg" src={logo}></img>
                 </div>
             </div>
             <div className="flex justify-between w-5/7">
