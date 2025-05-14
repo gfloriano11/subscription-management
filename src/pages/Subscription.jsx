d68422b1678768a5cc8a3e0366cdebe79e87882b
gfloriano11 <gflorianodev@gmail.com>
2025-05-14 13:44:22 -0300

    feat: create subscription information page

---
+++ src/pages/Subscription.jsx
@@ -0,0 +1,14 @@
+function Subscription(){
+
+    return(
+        <div>
+            <div className="bg-zinc-900 p-4 rounded-lg 
+            shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
+            text-white font-inter">
+                <p>Netflix</p>
+            </div>
+        </div>
+    );
+}
+
+export default Subscription;
