c3de3cdbec355415b52ff301719b0cd7a3c7d84a
gfloriano11 <gflorianodev@gmail.com>
2025-05-24 17:35:23 -0300

    feat: get subscription by id is ready to use

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 function Subscription(){
+
+    const [subscription, setSubscription] = useState([]);
+
+    const location = useLocation();
+    const id = location.state.id;
 
     async function getSubscription(id){
 
-        const response = await fetch(`http://localhost:8000/`)
 
+        const response = await fetch(`http://localhost:8000/my-subscriptions/${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        let data = await response.json();
+
+        console.log(data);
+
+        setSubscription(data[0]);
     }
+
+    useEffect(() => {
+        getSubscription(id);
+    }, [])
 
     return(
         <div>
             <div className="bg-zinc-900 p-4 rounded-lg 
             shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
             text-white font-inter">
-                <p>Netflix</p>
+                <p>{subscription.subscription_name}</p>
             </div>
         </div>
     );
