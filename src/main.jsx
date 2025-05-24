e7842c1630e88efc551237884bb3f1139fae32bc
gfloriano11 <gflorianodev@gmail.com>
2025-05-24 17:17:02 -0300

    fix: new subscription path read subscription page

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
       { path: 'add', element: <SelectCategory/> },
       { path: 'add/:category', element: <AddSubscription/> },
       { path: 'add/:category/:subscription', element: <AddSubscriptionInfo/> },
-      { path: ':subscription', element: <Subscription/>}
+      { path: ':subscription/:id', element: <Subscription/>}
     ]
   }
 ])
