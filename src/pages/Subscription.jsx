ad9e574aa7e66673873cfdf2fca7601db09a9ed6
gfloriano11 <gflorianodev@gmail.com>
2025-05-29 16:10:26 -0300

    style: subscription page information

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -33,24 +33,23 @@ function Subscription(){
 
     return(
         <section>
-            <div className="bg-zinc-900 p-4 rounded-lg 
+            <div className="bg-zinc-900 p-4 pr-0 rounded-lg 
             shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
             text-white font-inter flex flex-col md:flex-row gap-3">
                 <div className="relative">
                     <img className="rounded-xl max-w-xs md:max-w-md select-none pointer-events-none " src="\src\assets\subscription_image\netflix.png"></img>
                     <p className="absolute bottom-4 left-4 text-3xl font-bold tracking-wide">{subscription.subscription_name}</p>
                 </div>
-                <div className="flex flex-col justify-center gap-5">
-                    <div className="flex flex-col justify-between gap-3">
-                        <div className="flex flex-col">
-                            <p>Stats: Active</p>
-                            <p>Day for Payment: 06/27/2025</p>
-                            <p>Payment Method: Credit Card</p>
-                        </div>
-                        <div className="pr-2 flex flex-col items-center">
-                            <button>Editar</button>
-                            <button>Excluir</button>
-                        </div>
+                <div className="flex flex-col justify-center items-center gap-3">
+                    <div className="grid grid-cols-2 text-center md:text-start md:gap-x-3 gap-y-1">
+                        <p>📊 Status: Active</p>
+                        <p>📅 06/27/2025</p>
+                        <p>💳 Credit Card</p>
+                        <p>💵 R$39,99</p>
+                    </div>
+                    <div className="pr-2 flex flex-col items-center">
+                        <button>Editar</button>
+                        <button>Excluir</button>
                     </div>
                 </div>
             </div>
