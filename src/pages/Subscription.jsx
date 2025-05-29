ebd1816e8413dd52f1b2644a99f80ad1937804ca
gfloriano11 <gflorianodev@gmail.com>
2025-05-29 15:22:36 -0300

    style: subscrition name with image

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -35,15 +35,13 @@ function Subscription(){
         <section>
             <div className="bg-zinc-900 p-4 rounded-lg 
             shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
-            text-white font-inter flex gap-3">
-                <div className="max-w-xs md:max-w-md select-none pointer-events-none">
-                    <img className="rounded-xl" src="\src\assets\subscription_image\netflix.png"></img>
+            text-white font-inter flex flex-col md:flex-row gap-3">
+                <div className="relative">
+                    <img className="rounded-xl max-w-xs md:max-w-md select-none pointer-events-none " src="\src\assets\subscription_image\netflix.png"></img>
+                    <p className="absolute bottom-4 left-4 text-3xl font-bold tracking-wide">{subscription.subscription_name}</p>
                 </div>
-                <div className="flex flex-col">
-                    <div className="text-center font-medium text-2xl tracking-wide">
-                        <p>{subscription.subscription_name}</p>
-                    </div>
-                    <div className="flex flex-col justify-center gap-3">
+                <div className="flex flex-col justify-center gap-5">
+                    <div className="flex flex-col justify-between gap-3">
                         <div className="flex flex-col">
                             <p>Stats: Active</p>
                             <p>Day for Payment: 06/27/2025</p>
