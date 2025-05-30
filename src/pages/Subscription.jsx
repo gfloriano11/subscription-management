ea713c4fa274dd2e302daacf1b47e4f916b053c3
gfloriano11 <gflorianodev@gmail.com>
2025-05-30 16:11:05 -0300

    style: remove max width from mobile subscription image

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -51,7 +51,7 @@ function Subscription(){
             shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
             text-white font-inter flex flex-col md:flex-row gap-3">
                 <div className="relative">
-                    <img className="rounded-xl max-w-xs max-h-64 md:max-w-md select-none pointer-events-none" src={subscription.image}></img>
+                    <img className="rounded-xl w-full max-h-64 md:max-w-md select-none pointer-events-none" src={subscription.image}></img>
                     <p className="absolute bottom-4 left-4 text-3xl font-bold tracking-wide">{subscription.subscription_name}</p>
                 </div>
                 <div className="flex flex-col justify-center items-center gap-3">
