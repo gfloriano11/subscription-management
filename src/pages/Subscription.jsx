906fad8c2d05cfedadb994316de9b6fd9d08a70c
gfloriano11 <gflorianodev@gmail.com>
2025-06-12 14:50:45 -0300

    style: add subscription image black background color

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -50,7 +50,7 @@ function Subscription(){
             shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800 
             text-white font-inter flex flex-col md:flex-row gap-3">
                 <div className="relative flex items-center md:h-64">
-                    <img className="rounded-xl w-full max-h-64 md:max-w-md select-none pointer-events-none" src={subscription.image}></img>
+                    <img className="bg-black rounded-xl w-full max-h-64 md:max-w-md select-none pointer-events-none" src={subscription.image}></img>
                     <p className="absolute bottom-4 left-4 text-3xl font-bold tracking-wide">{subscription.subscription_name}</p>
                 </div>
                 <div className="flex flex-col justify-center items-center gap-3">
