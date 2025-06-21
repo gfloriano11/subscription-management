f8120b8dca42b7884569bd693a20189ad203c65f
gfloriano11 <gflorianodev@gmail.com>
2025-06-21 18:14:27 -0300

    feat: add pop up to delete subscription

---
+++ src/components/PopUpDelete.jsx
@@ -0,0 +1,23 @@
+import { X } from "lucide-react";
+
+function PopUp({text, error, onClick}){
+
+    return(
+        <div className="absolute top-10 z-20 bg-zinc-900 p-4 rounded-lg 
+        shadow-[0px_10px_20px] shadow-purple-950 border-2 border-gray-800">
+            <div className="absolute right-2 top-2 flex justify-end cursor-pointer">
+                <X onClick={onClick}/>
+            </div>
+            <div className="p-3 text-white">
+                <div>
+                    <p className="text-2xl">{text}</p>
+                </div>
+                <div className="text-center">
+                    <p>{error}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PopUp;
