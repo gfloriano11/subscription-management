5e2816062978b8d213391b700dbb357aab9ea944
gfloriano11 <gflorianodev@gmail.com>
2025-07-03 17:55:35 -0300

    fix: routes to menu page

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,10 @@ const router = createBrowserRouter([
     element: <Menu/>
   },
   {
+    path: '/menu',
+    element: <Menu/>
+  },
+  {
     path: '/home',
     element: <App/>,
     children: [
