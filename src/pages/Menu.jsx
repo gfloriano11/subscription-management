5b6f8e345593d776fa6d7d91b25231b269dff802
gfloriano11 <gflorianodev@gmail.com>
2025-07-05 11:06:50 -0300

    style: add new text in landing page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,9 +4,15 @@ function Menu(){
 
     return(
         <div className="flex justify-center items-center h-dvh bg-slate-950 text-blue-50">
-            <div className="max-w-100 text-5xl font-wf-sans font-semibold">
-                <p className="text-violet-400">Control your accounts.</p>
-                <p>Manage your expanses!</p>
+            <div className="flex flex-col gap-2">
+                <div className="max-w-100 text-5xl font-wf-sans font-semibold">
+                    <p className="text-violet-400">Control your accounts.</p>
+                    <p>Manage your expanses!</p>
+                </div>
+                <div className="max-w-90 text-base font-wf-sans font-normal">
+                    <p>Create your account now, and discover where you are spenting your money.</p>
+                    <p>Remember when your accounts are due.</p>
+                </div>
             </div>
             <div className="flex gap-2">
                 <Link
