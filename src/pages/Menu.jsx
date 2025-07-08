912180b437e8de237e8c84ca35a053db937fa13f
gfloriano11 <gflorianodev@gmail.com>
2025-07-08 18:51:13 -0300

    style: add max width in login and register button

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -5,19 +5,18 @@ import { CreditCard } from "lucide-react";
 function Menu(){
 
     return(
-        <section className="flex flex-col justify-center items-center gap-5 h-dvh bg-slate-950 text-blue-50">
-            <section className="flex justify-center items-center">
-                <div className="flex flex-col gap-2">
+        <section className="bg-slate-950 text-blue-50">
+            <section className="flex flex-col md:items-center md:flex-row min-h-dvh justify-center px-6 py-20 gap-6 max-w-7xl mx-auto">
+                <div className="flex flex-col gap-3 max-w-xl">
                     <div className="max-w-100 text-5xl font-wf-sans font-semibold">
                         <p className="text-violet-400">Control your accounts.</p>
                         <p>Manage your expanses!</p>
                     </div>
-                    <div className="max-w-90 text-base font-wf-sans font-normal">
-                        <p>Create your account now, and discover where you are spenting your money.</p>
-                        <p>Remember when your accounts are due.</p>
+                    <div className="text-lg max-w-100 text-slate-300 font-wf-sans">
+                        <p>Create your account now, and discover where your money goes.</p>
                     </div>
                 </div>
-                <div className="flex gap-5">
+                <div className="flex flex-col max-w-2xs sm:flex-row gap-5">
                     <LandingPageButton link='/login' text='Login' color='text-white' background='bg-violet-700' border='border-zinc-900'/>
                     <LandingPageButton link='/register' text='Register' color='text-black' background='bg-slate-300' border='border-black'/>
                 </div>
