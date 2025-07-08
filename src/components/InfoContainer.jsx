ad19d2803b30fa016df8a78ac4de71ad020280e4
gfloriano11 <gflorianodev@gmail.com>
2025-07-08 18:28:12 -0300

    style: info container

--- src/components/InfoContainer.jsx
+++ src/components/InfoContainer.jsx
@@ -1,8 +1,8 @@
 function InfoContainer({icon, text}){
     return(
-        <div>
-            <div className="max-w-12 h-12 rounded-3xl bg-gray-300 flex justify-center items-center">{icon}</div>
-            <div className="bg-gray-300"><p>{text}</p></div>
+        <div className="w-3/5 flex flex-col items-center relative">
+            <div className="absolute -top-5 max-w-12 w-full h-12 rounded-3xl bg-gray-200 shadow-md shadow-gray-400 flex justify-center items-center border border-neutral-700">{icon}</div>
+            <div className="bg-gray-200 pt-5 text-center w-full rounded-md text-black"><p>{text}</p></div>
         </div>
     )
 }
