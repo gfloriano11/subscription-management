349568a5820f11f9f3156de59de9a1b435ebe6d4
gfloriano11 <gflorianodev@gmail.com>
2025-07-09 10:24:11 -0300

    style: change ui from info card

--- src/components/InfoContainer.jsx
+++ src/components/InfoContainer.jsx
@@ -1,8 +1,8 @@
 function InfoContainer({icon, text}){
     return(
-        <div className="w-3/5 flex flex-col items-center relative">
-            <div className="absolute -top-5 max-w-12 w-full h-12 rounded-3xl bg-gray-200 shadow-md shadow-gray-400 flex justify-center items-center border border-neutral-700">{icon}</div>
-            <div className="bg-gray-200 pt-5 text-center w-full rounded-md text-black"><p>{text}</p></div>
+        <div className="w-3/5 h-40 flex flex-col justify-around items-center bg-gray-200 p-4 rounded-md">
+            <div className="max-w-12 w-full h-12 rounded-3xl shadow-md shadow-gray-400 flex justify-center items-center border bg-blue-100 border-neutral-300">{icon}</div>
+            <div className="text-center w-full rounded-md text-black"><p>{text}</p></div>
         </div>
     )
 }
