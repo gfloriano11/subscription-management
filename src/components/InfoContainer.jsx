329c2ad774b11564a166e90f83a2953d09483439
gfloriano11 <gflorianodev@gmail.com>
2025-07-10 11:33:02 -0300

    feat: add props to manipulate container

--- src/components/InfoContainer.jsx
+++ src/components/InfoContainer.jsx
@@ -1,7 +1,7 @@
-function InfoContainer({icon, text}){
+function InfoContainer({icon, text, border, shadow, background}){
     return(
-        <div className="w-3/5 h-40 flex flex-col justify-around items-center bg-gray-200 p-4 rounded-md">
-            <div className="max-w-12 w-full h-12 rounded-3xl shadow-md shadow-gray-400 flex justify-center items-center border bg-blue-100 border-neutral-300">{icon}</div>
+        <div className="w-3/5 h-40 flex flex-col justify-around items-center bg-gray-100 p-4 rounded-md">
+            <div className={`max-w-12 w-full h-12 rounded-3xl shadow-md ${shadow} flex justify-center items-center border ${background} ${border}`}>{icon}</div>
             <div className="text-center w-full rounded-md text-black"><p>{text}</p></div>
         </div>
     )
