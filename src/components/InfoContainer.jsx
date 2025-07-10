7c5f6d3a1c5d11e42c8dde48258c60818b6bf675
gfloriano11 <gflorianodev@gmail.com>
2025-07-10 15:51:19 -0300

    style: change background color

--- src/components/InfoContainer.jsx
+++ src/components/InfoContainer.jsx
@@ -1,6 +1,6 @@
 function InfoContainer({icon, text, border, shadow, background}){
     return(
-        <div className="w-3/5 h-40 flex flex-col justify-around items-center bg-gray-100 p-4 rounded-md">
+        <div className="w-3/5 h-40 flex flex-col justify-around items-center bg-gray-50 p-4 rounded-md">
             <div className={`max-w-12 w-full h-12 rounded-3xl shadow-md ${shadow} flex justify-center items-center border ${background} ${border}`}>{icon}</div>
             <div className="text-center w-full rounded-md text-black"><p>{text}</p></div>
         </div>
