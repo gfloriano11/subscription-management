7b38c0b1c483b7fd87d4ccabaae270ae673d1a16
gfloriano11 <gflorianodev@gmail.com>
2025-07-13 13:10:20 -0300

    feat: add hover animation in buttons

--- src/components/LandingPageButton.jsx
+++ src/components/LandingPageButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function LandingPageButton({text, link, color, background, border}){
     return(
-        <Link className={`${color} tracking-wide font-semibold ${background} min-w-35 max-h-12 p-2.5 pr-4 pl-4 border ${border} rounded-lg text-center`} 
+        <Link className={`hover:scale-105 duration-150 ${color} tracking-wide font-semibold ${background} min-w-35 max-h-12 p-2.5 pr-4 pl-4 border ${border} rounded-lg text-center`} 
         to={link}>
             <p>{text}</p>
         </Link>
