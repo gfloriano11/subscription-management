55758722c66f0d733a7fb81911bbd8fd4fb9eaa0
gfloriano11 <gflorianodev@gmail.com>
2025-07-16 18:22:21 -0300

    feat: use auth components

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import Input from "../components/Auth/Input";
+import EnterButton from "../components/Auth/EnterButton";
 
 function Login(){
 
@@ -7,21 +9,11 @@ function Login(){
             <div className="ml-5 mr-5 flex flex-col w-full max-w-md bg-slate-900/80 backdrop-blur-md p-8 rounded-2xl border border-slate-800 shadow-xl gap-3">
                 <h2 className="text-2xl font-semibold text-white mb-3 text-center">Enter in your account:</h2>
                 <div className="flex flex-col gap-3">
-                    <input
-                    className="p-3 rounded-md bg-slate-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-700" 
-                    type="text"
-                    placeholder="Your best e-mail :)"
-                    />
-                    <input
-                    className="p-3 rounded-md bg-slate-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-700" 
-                    type="password"
-                    placeholder="Add your password"
-                    />
+                    <Input type="text" placeholder="Your best e-mail :)"/>
+                    <Input type="password" placeholder="Add your password"/>
                 </div>
                 <div className="flex flex-col gap-2">
-                    <button className="w-full hover:cursor-pointer bg-violet-700 hover:bg-violet-800 text-white font-medium py-3 rounded-md transition">
-                        <p>Enter</p>
-                    </button>
+                    <EnterButton text="Login"/>
                     <Link to='/register'><p className="text-gray-200 text-center">Don't have an account? Register here</p></Link>
                 </div>
             </div>
