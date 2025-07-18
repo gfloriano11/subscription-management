f9dfe9fea1435f0ebd18b16b5ec50bd984bd7582
gfloriano11 <gflorianodev@gmail.com>
2025-07-18 18:05:15 -0300

    feat: add show and unshow password

--- src/components/Auth/Input.jsx
+++ src/components/Auth/Input.jsx
@@ -1,7 +1,35 @@
-function Input({type, placeholder}){
+import { Eye, EyeClosed } from "lucide-react";
+
+function Input({type, placeholder, password, showPass, setShowPass}){
+
+    if(password){
+
+        if(showPass){
+            type='text'
+        }
+
+        return(
+        <div className="flex items-center p-3 rounded-md bg-slate-800 focus-within:ring-2 focus-within:ring-violet-700">
+            <input
+                className="w-full text-white placeholder-gray-400 focus:outline-none"
+                type={`${type}`}
+                placeholder={`${placeholder}`}
+            />
+            {showPass
+            ?
+            <EyeClosed className="hover:cursor-pointer"
+            onClick={(() => setShowPass(false))}/>
+            :
+            <Eye className="hover:cursor-pointer"
+            onClick={(() => setShowPass(true))}/>
+            }
+        </div>
+        
+    )
+    }
     return(
         <input
-            className="p-3 rounded-md bg-slate-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-700" 
+            className="w-full p-3 rounded-md bg-slate-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-700" 
             type={`${type}`}
             placeholder={`${placeholder}`}
         />
