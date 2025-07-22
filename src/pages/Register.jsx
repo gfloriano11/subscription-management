e23499aadbc66cdc8a7c8c33d2f38f323919a8fa
gfloriano11 <gflorianodev@gmail.com>
2025-07-22 18:40:29 -0300

    fix: register page strucute with stages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,26 +10,25 @@ function Register(){
     const [password, setPassword] = useState('');
     const [showPass, setShowPass] = useState(false);
 
-    if(stage === 1){
-        return(
-            <section className="bg-slate-950 min-h-dvh flex justify-center items-center text-blue-50">
-            <div className="ml-5 mr-5 flex flex-col w-full max-w-md bg-slate-900/80 backdrop-blur-md p-8 rounded-2xl border border-slate-800 shadow-xl gap-3">
+    return(
+        <section className="bg-slate-950 min-h-dvh flex justify-center items-center text-blue-50">
+            <div className="ml-5 mr-5 flex flex-col w-full max-w-md bg-slate-900/80 backdrop-blur-md p-8 rounded-2xl border border-slate-800 shadow-xl">
                 <h2 className="text-2xl font-semibold text-white mb-3 text-center">Create your account:</h2>
-                <div className="flex flex-col gap-3">
-                    <Input placeholder="Your best e-mail :)" type="text" onChange={(event) => setEmail(event.target.value)}/>
-                    <Input placeholder="Add your password" type="password" onChange={(event) => setPassword(event.target.value)} showPass={showPass} setShowPass={setShowPass}/>
+                    {stage == 1 &&(
+                    <div className="w-full flex flex-col gap-3">
+                        <div className="flex flex-col gap-3">
+                            <Input placeholder="Your best e-mail :)" type="text" onChange={(event) => setEmail(event.target.value)}/>
+                            <Input placeholder="Add your password" type="password" onChange={(event) => setPassword(event.target.value)} showPass={showPass} setShowPass={setShowPass}/>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <NextPageButton onClick={() => setStage(2)}/>
+                            <Link to='/login'><p className="text-gray-200 text-center">Already have an account? Enter here</p></Link>
+                        </div>
+                    </div>
+                    )}
                 </div>
-                <div className="flex flex-col gap-2">
-                    <NextPageButton/>
-                    <Link to='/login'><p className="text-gray-200 text-center">Already have an account? Enter here</p></Link>
-                </div>
-            </div>
         </section>
-        )
-    }
-
-    // return(
-    // )
+    )
 }
 
 export default Register;
