98416e134d2a05f05d2ed2eb5918c4598ee5f9af
gfloriano11 <gflorianodev@gmail.com>
2025-07-22 18:40:49 -0300

    feat: add onClick in next page button to change stage page

--- src/components/Auth/NextPageButton.jsx
+++ src/components/Auth/NextPageButton.jsx
@@ -1,8 +1,9 @@
 import { ArrowRight } from "lucide-react";
 
-function NextPageButton(){
+function NextPageButton({onClick}){
     return(
-        <button className="w-full flex justify-center group hover:cursor-pointer bg-violet-700 hover:bg-violet-800 text-white font-medium py-3 rounded-md transition">
+        <button className="w-full flex justify-center group hover:cursor-pointer bg-violet-700 hover:bg-violet-800 text-white font-medium py-3 rounded-md transition"
+        onClick={onClick}>
             <ArrowRight className="transition-transform duration-500 group-hover:translate-x-2 group-hover:scale-115 "/>
         </button>
     )
