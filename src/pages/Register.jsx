5c9edeb8dd724b2bebdd70285507e5d3ba9360ff
gfloriano11 <gflorianodev@gmail.com>
2025-07-22 18:42:05 -0300

    style: add gap between title and inputs

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,7 +12,7 @@ function Register(){
 
     return(
         <section className="bg-slate-950 min-h-dvh flex justify-center items-center text-blue-50">
-            <div className="ml-5 mr-5 flex flex-col w-full max-w-md bg-slate-900/80 backdrop-blur-md p-8 rounded-2xl border border-slate-800 shadow-xl">
+            <div className="ml-5 mr-5 flex flex-col w-full max-w-md bg-slate-900/80 backdrop-blur-md p-8 rounded-2xl border border-slate-800 shadow-xl gap-3">
                 <h2 className="text-2xl font-semibold text-white mb-3 text-center">Create your account:</h2>
                     {stage == 1 &&(
                     <div className="w-full flex flex-col gap-3">
