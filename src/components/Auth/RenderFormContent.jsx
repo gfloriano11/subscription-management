2ccb2f63b055d863e2a6cfb72885b892bfc3f8b0
gfloriano11 <gflorianodev@gmail.com>
2025-07-23 18:04:30 -0300

    feat: function to render form

---
+++ src/components/Auth/RenderFormContent.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Input from "../../components/Auth/Input";
+import NextPageButton from "../../components/Auth/NextPageButton";
+import { Link } from "react-router-dom";
+
+function RenderFormContent({stage, setStage, setEmail, setPassword, showPass, setShowPass}){
+    if(stage === 1){
+        return(
+            <div className="w-full flex flex-col gap-3">
+                    <div className="flex flex-col gap-3">
+                        <Input placeholder="Your best e-mail :)" type="text" onChange={(event) => setEmail(event.target.value)}/>
+                        <Input placeholder="Add your password" type="password" onChange={(event) => setPassword(event.target.value)} showPass={showPass} setShowPass={setShowPass}/>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                    <NextPageButton onClick={() => setStage(2)}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default RenderFormContent;
