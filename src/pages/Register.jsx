96e7f66a0e9a2823e2380b93f66b3e72b0ce3255
gfloriano11 <gflorianodev@gmail.com>
2025-07-31 17:54:37 -0300

    feat: create function to register user [frontend]

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,6 +11,18 @@ function Register(){
     const [salary, setSalary] = useState(0);
     const [showPass, setShowPass] = useState(false);
 
+    async function registerUser(){
+        const userData = { name, email, password, age, salary };
+
+        const response = await fetch(`http://localhost:8000/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+    }
+
     return(
         <section className="bg-slate-950 min-h-dvh flex justify-center items-center text-blue-50">
             <div className="ml-5 mr-5 flex flex-col w-full max-w-md bg-slate-900/80 backdrop-blur-md p-8 rounded-2xl border border-slate-800 shadow-xl gap-3">
@@ -24,6 +36,7 @@ function Register(){
                     setSalary={setSalary}
                     showPass={showPass}
                     setShowPass={setShowPass}
+                    registerUser={registerUser}
                 />
                 <Link to='/login'><p className="text-gray-200 text-center">Already have an account? Enter here</p></Link>
                 </div>
