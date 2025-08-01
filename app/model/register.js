eafa4c1d140002703f5223e6618075df622db121
gfloriano11 <gflorianodev@gmail.com>
2025-08-01 19:15:26 -0300

    feat: hash user password

--- app/model/register.js
+++ app/model/register.js
@@ -1,8 +1,38 @@
-import connect from "../connection/connection";
+import connect from "../connection/connection.js";
+import bcrypt, { hash } from 'bcrypt';
 
 function addUser(req, res){
 
+    const userData = req.body;
+    let hashPass;
+
+    bcrypt.hash(userData.password, 10, (error, hash) => {
+        if(error){
+            throw error; 
+        }
+
+        hashPass = hash;
+
+        console.log('hashed pass: ', hashPass);
+
+        bcrypt.compare(userData.password, hashPass, (error, result) => {
+            console.log('verificando...');
+            if(result){
+                console.log('senha correta: ', userData.password);
+            }
+        })
+    });
+
+    
+
     const connection = connect.getConnection();
 
-    const query = `INSERT INTO users`
+    const query = `INSERT INTO users
+    (fullname, email, password_hash, salary, age)
+    VALUES
+    (?, ?, ?, ?, ?)`;
+}
+
+export default {
+    addUser
 }
