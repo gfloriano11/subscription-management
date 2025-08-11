00dfb521fe24cbf384dbbc1e1c997ce4f7569058
gfloriano11 <gflorianodev@gmail.com>
2025-08-10 21:50:07 -0300

    chore: add onClick in login button

--- src/components/Auth/EnterButton.jsx
+++ src/components/Auth/EnterButton.jsx
@@ -1,6 +1,8 @@
-function EnterButton({text}){
+function EnterButton({text, onClick}){
     return(
-        <button className="w-full hover:cursor-pointer bg-violet-700 hover:bg-violet-800 text-white font-medium py-3 rounded-md transition">
+        <button
+        onClick={onClick} 
+        className="w-full hover:cursor-pointer bg-violet-700 hover:bg-violet-800 text-white font-medium py-3 rounded-md transition">
             <p>{text}</p>
         </button>
     )
