cfafb3347a92c636b3ff4dba8a77ab55d8d606ad
gfloriano11 <gflorianodev@gmail.com>
2025-08-10 21:50:32 -0300

    feat: function to send requistion to login user

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,16 +8,28 @@ function Login(){
     const [password, setPassword] = useState('');
     const [showPass, setShowPass] = useState(false);
 
+    async function loginUser(){
+        const userData = { email, password };
+
+        const response = await fetch(`http://localhost:8000/user/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(userData)
+        })
+    }
+
     return(
         <section className="bg-slate-950 min-h-dvh flex justify-center items-center text-blue-50">
             <div className="ml-5 mr-5 flex flex-col w-full max-w-md bg-slate-900/80 backdrop-blur-md p-8 rounded-2xl border border-slate-800 shadow-xl gap-3">
                 <h2 className="text-2xl font-semibold text-white mb-3 text-center">Enter in your account:</h2>
                 <div className="flex flex-col gap-3">
-                    <Input type="text" placeholder="Your e-mail"/>
-                    <Input placeholder="Password" type="password" showPass={showPass} setShowPass={setShowPass}/>
+                    <Input name="email" type="text" placeholder="Your e-mail" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                    <Input name="password" placeholder="Password" type="password" showPass={showPass} setShowPass={setShowPass} value={password} onChange={(event) => setPassword(event.target.value)}/>
                 </div>
                 <div className="flex flex-col gap-3">
-                    <EnterButton text="Login"/>
+                    <EnterButton text="Login" onClick={() => loginUser()}/>
                     <Link to='/register'><p className="text-gray-200 text-center">Don't have an account? Register here</p></Link>
                 </div>
             </div>
