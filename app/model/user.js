9130dd93eb55a51d6dce8c87f15cd3735184162c
gfloriano11 <gflorianodev@gmail.com>
2025-08-14 16:04:56 -0300

    feat: add middleware function to token verification

--- app/model/user.js
+++ app/model/user.js
@@ -91,6 +91,22 @@ async function login(req, res){
 
 }
 
+async function authenticateUserToken(req, res, next){
+    const token = req.headers['authorization'];
+
+    if(!token){
+        return res.status(401).json({message: 'Token doesnt exist. Login again.'});
+    }
+
+    jwt.verify(token, secretKey, (error, result) => {
+        if(error){
+            return res.status(403).json({message: 'Invalid token.'});
+        }
+        req.user = user;
+        next();
+    })
+}
+
 export default {
     register,
     login
