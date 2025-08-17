04bfe441e9f5cbcc99803d1452782406422fa49b
gfloriano11 <gflorianodev@gmail.com>
2025-08-16 21:40:12 -0300

    fix: add middleware folder

---
+++ app/middlewares/auth.js
@@ -0,0 +1,15 @@
+async function authenticateUserToken(req, res, next){
+    const token = req.headers['authorization'];
+
+    if(!token){
+        return res.status(401).json({message: 'Token doesnt exist. Login again.'});
+    }
+
+    jwt.verify(token, secretKey, (error, result) => {
+        if(error){
+            return res.status(403).json({message: 'Invalid token.'});
+        }
+        req.user = user;
+        next();
+    })
+}
--- app/model/user.js
+++ app/model/user.js
@@ -91,22 +91,6 @@ async function login(req, res){
 
 }
 
-async function authenticateUserToken(req, res, next){
-    const token = req.headers['authorization'];
-
-    if(!token){
-        return res.status(401).json({message: 'Token doesnt exist. Login again.'});
-    }
-
-    jwt.verify(token, secretKey, (error, result) => {
-        if(error){
-            return res.status(403).json({message: 'Invalid token.'});
-        }
-        req.user = user;
-        next();
-    })
-}
-
 export default {
     register,
     login
