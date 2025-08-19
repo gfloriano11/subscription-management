e75cb704cd8821c0f703f9a2f8d8b8000612105b
gfloriano11 <gflorianodev@gmail.com>
2025-08-19 09:55:09 -0300

    feat: send user to req params to have jwt info

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -14,10 +14,11 @@ async function authenticateUserToken(req, res, next){
         return res.status(401).json({message: 'Token doesnt exist. Login again.'});
     }
 
-    jwt.verify(token, secretKey, (error, result) => {
+    jwt.verify(token, secretKey, (error, user) => {
         if(error){
             return res.status(403).json({message: 'Invalid token.'});
         }
+
         req.user = user;
         next();
     })
