0a0f58954d6b0051d62f2d300c24ea89b800adfc
gfloriano11 <gflorianodev@gmail.com>
2025-08-27 21:12:10 -0300

    style: add user icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
+import { CircleUserRound } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
     const navigate = useNavigate();
 
     return (
-        <nav className="w-full bg-slate-950 p-4 flex border border-b-gray-800 justify-end items-center shadow-sm shadow-violet-950 fixed top-0 left-0 z-50">
+        <nav className="w-full bg-slate-950/0 p-4 flex justify-end items-center fixed top-0 left-0 z-50">
             <button
                 onClick={() => navigate('/profile')}
                 className="focus:outline-none"
             >
                 <img
-                    src="/icons/user.svg"
+                    src="src/assets/user/user_black.svg"
                     alt="Perfil do usuário"
-                    className="w-8 h-8 rounded-full hover:ring-2 hover:ring-white transition"
+                    className="bg-gray-200 pt-0.5 w-8 h-8 rounded-full hover:ring-2 hover:ring-white transition"
                 />
             </button>
         </nav>
