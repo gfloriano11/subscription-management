c88e96686526e36adc140571fe4a04c56221871b
gfloriano11 <gflorianodev@gmail.com>
2025-08-30 15:10:16 -0300

    fix: change user icon to Link component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,18 @@
 import { CircleUserRound } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
     const navigate = useNavigate();
 
     return (
         <nav className="w-full bg-slate-950/0 p-4 pointer-events-none flex justify-end items-center fixed top-0 left-0 z-50">
-            <button
-                onClick={() => navigate('/profile')}
-                className="focus:outline-none"
-            >
+            <Link to="/profile">
                 <img
                     src="src/assets/user/user_black.svg"
                     alt="Perfil do usuário"
-                    className="bg-gray-200 pointer-events-auto pt-0.5 w-8 h-8 rounded-full hover:ring-2 hover:ring-white transition"
+                    className="bg-gray-200 pointer-events-auto pt-1 w-10 h-10 rounded-full hover:cursor-pointer hover:scale-105 transition duration-500"
                 />
-            </button>
+            </Link>
         </nav>
     );
 }
