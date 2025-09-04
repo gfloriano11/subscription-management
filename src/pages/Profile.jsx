d53bb1c2b568a4a14b9b6d579da5787f84de8f13
gfloriano11 <gflorianodev@gmail.com>
2025-09-03 22:51:17 -0300

    feat: start requistion to get user data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 function Profile(){
+
+    const navigate = useNavigate();
+    async function getUser(){
+
+        const token = localStorage.getItem('token'); 
+        
+        if(!token){
+            navigate('/login');
+        }
+        
+        const response = await fetch(`http://localhost:8000/user`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            } 
+        })
+    }
     return(
         <>
             <h1 className="text-white">Welcome, username!</h1>
