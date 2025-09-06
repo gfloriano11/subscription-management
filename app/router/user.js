47cefb705becdb5ad3643c0389baf6bfe72117e8
gfloriano11 <gflorianodev@gmail.com>
2025-09-06 15:52:19 -0300

    fix: route to get user data

--- app/router/user.js
+++ app/router/user.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 
 userRouter.post('/register', user.register);
 userRouter.post('/login', user.login);
-userRouter.get('/:id', authenticate, user.getUserData);
+userRouter.get('/', authenticate, user.getUserData);
 
 export default userRouter;
