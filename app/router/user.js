1ce44e17a511fb29b079bd2068064f678a504691
gfloriano11 <gflorianodev@gmail.com>
2025-09-05 23:00:02 -0300

    chore: authenticate user in profile function

--- app/router/user.js
+++ app/router/user.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import user from '../model/user.js';
+import authenticate from '../middlewares/auth.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', user.register);
 userRouter.post('/login', user.login);
-userRouter.get('/:id', user.getUserData);
+userRouter.get('/:id', authenticate, user.getUserData);
 
 export default userRouter;
