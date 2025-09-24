a2b81e14b0605b570dbccd17a207776fc899ed31
gfloriano11 <gflorianodev@gmail.com>
2025-09-24 17:20:07 -0300

    style: create style for error and default input

--- src/components/Auth/Input.jsx
+++ src/components/Auth/Input.jsx
@@ -1,13 +1,16 @@
 import { Eye, EyeClosed } from "lucide-react";
 
-function Input({type, name, value, placeholder, showPass, setShowPass, onChange, children}){
+function Input({type, name, value, placeholder, showPass, setShowPass, onChange, children, error}){
+
+    const defaultStyle = `w-full p-3 rounded-md bg-slate-800 text-white outline-red-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-700`;
+    const errorStyle = `transition-all duration-500 w-full p-3 rounded-md bg-slate-800 text-white outline-1 outline-red-800 placeholder-gray-400 focus:outline-red-800`;
 
     if (type === "select"){
         return (
           <select
             onChange={onChange}
             value={value}
-            className="w-full h-12 p-3 rounded-md bg-slate-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-700" 
+            className={error ? errorStyle : `h-12 ${defaultStyle}`}
           >
             {children}
           </select>
@@ -21,7 +24,7 @@ function Input({type, name, value, placeholder, showPass, setShowPass, onChange,
         }
 
         return(
-        <div className="flex items-center p-3 rounded-md bg-slate-800 focus-within:ring-2 focus-within:ring-violet-700">
+        <div className={`flex items-center ${error ? errorStyle : `${defaultStyle} focus-within:ring-2 focus-within:ring-violet-700`}`}>
             <input
                 name={name}
                 className="w-full text-white placeholder-gray-400 focus:outline-none"
@@ -45,7 +48,7 @@ function Input({type, name, value, placeholder, showPass, setShowPass, onChange,
     return(
         <input
             name={name}
-            className="w-full p-3 rounded-md bg-slate-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-violet-700" 
+            className={error ? errorStyle : defaultStyle}
             type={`${type}`}
             placeholder={`${placeholder}`}
             value={value}
