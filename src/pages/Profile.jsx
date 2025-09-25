db04e8aad739cf94a8f803500b0963b391c73c58
gfloriano11 <gflorianodev@gmail.com>
2025-09-25 17:25:22 -0300

    feat: add navbar to user profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,4 @@
+import { ArrowLeft } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -30,8 +31,23 @@ function Profile(){
     }, []);
     
     return(
-        <section className="flex justify-center items-center">
-            <h1 className="text-white text-2xl font-inter">Welcome, <span className="font-semibold">{`${user.fullname}`}</span></h1>
+        <section className="flex flex-col w-full h-dvh">
+            <nav className="flex justify-between text-white p-3"> 
+                <div className="gap-3 flex items-center">
+                    <ArrowLeft size={28}/>
+                    <p className="text-lg font-semibold">{user.fullname}</p>
+                </div>
+                <div>
+                    <img
+                        src="src/assets/user/user_black.svg"
+                        alt="Perfil do usuário"
+                        className="bg-gray-200 pointer-events-auto pt-1 w-10 h-10 rounded-full hover:cursor-pointer hover:scale-105 transition duration-500"
+                    />
+                </div>
+            </nav>
+            <div className="w-full h-full flex flex-col justify-center items-center">
+                <h1 className="text-white text-2xl font-inter">Welcome, <span className="font-semibold">{`${user.fullname}`}</span></h1>
+            </div>
         </section>
     );
 }
